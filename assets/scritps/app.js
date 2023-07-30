e606e965b9a045e256b16662a9de1fcd45e654cf
Musa Shaba <musamohshaba@gmail.com>
2023-07-30 06:34:01 +0100

    Done With this shitty Designs

--- assets/scritps/app.js
+++ assets/scritps/app.js
@@ -1,5 +1,6 @@
 const shareBtn = document.getElementById('share');
-
+const profile = document.querySelector('.profile');
 function showTooltip() {
   shareBtn.classList.toggle('active');
+  profile.classList.toggle('active');
 }
